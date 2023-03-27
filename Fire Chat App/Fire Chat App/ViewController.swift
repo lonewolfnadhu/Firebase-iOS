338dada9de99f3cbef59a3b2041bb3ac0bf82d0d
Nadir Shah <lonewolfnadhu@gmail.com>
2023-03-27 19:34:35 +0100

    Initial files added

---
+++ Fire Chat App/Fire Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fire Chat App
+//
+//  Created by Nadir Shah on 27/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
