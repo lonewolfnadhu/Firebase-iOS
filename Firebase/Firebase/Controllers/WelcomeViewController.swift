a5ff3987d085ec51e3e8420b8fcfd448a3096e6b
Nadir Shah <lonewolfnadhu@gmail.com>
2023-04-30 18:53:37 +0100

    CLTypingLabel implemented

--- Firebase/Firebase/Controllers/WelcomeViewController.swift
+++ Firebase/Firebase/Controllers/WelcomeViewController.swift
@@ -6,36 +6,43 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
+        titleLabel.text = "⚡️FlashChat"
+        
+//        flashLabelAnimation()
+    }
+    
+//    func flashLabelAnimation(){
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText {
 //            print("-")
 //            print(0.1 * charIndex)
 //            print(letter)
-            
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
-    }
+//
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (Timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
+//    }
     
     @IBAction func registerPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToRegisterVC", sender: self)
-//        print("Register Pressed!")
+        //        print("Register Pressed!")
     }
     
     @IBAction func loginPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToLoginVC", sender: self)
-//        print("Login Pressed!")
+        //        print("Login Pressed!")
     }
 }
