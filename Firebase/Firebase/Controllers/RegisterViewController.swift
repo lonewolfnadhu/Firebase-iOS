d3e9a24f63cfd21cff681d39e348fd699585436b
Nadir Shah <lonewolfnadhu@gmail.com>
2023-05-01 08:58:55 +0100

    Firebase auth implemented with user registration

--- Firebase/Firebase/Controllers/RegisterViewController.swift
+++ Firebase/Firebase/Controllers/RegisterViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     
@@ -13,6 +14,16 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print("Firebase Auth (Error):", e.localizedDescription)
+                } else {
+                    print("Firebase Auth (Success)!")
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        }
     }
-    
 }
